184e923cdc8407a5a9973cec06cbfbb2cb411d21
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2011-08-31 16:21:52 +0200

    Code cleanup/refactoring

--- code/Model/Observer.php
+++ code/Model/Observer.php
@@ -4,6 +4,7 @@
  *
  * @category    Aoe
  * @package     Aoe_Static
+ * @author		Fabrizio Branca <mail@fabrizio-branca.de>
  * @author      Toni Grigoriu <toni@tonigrigoriu.com>
  */
 class Aoe_Static_Model_Observer
@@ -16,28 +17,20 @@ class Aoe_Static_Model_Observer
      */
     public function processPreDispatch(Varien_Event_Observer $observer)
     {
-        /* @var $helper Aoe_Static_Helper_Data */
-        $helper = Mage::helper('aoestatic');
         
+        $helper = Mage::helper('aoestatic'); /* @var $helper Aoe_Static_Helper_Data */
         $controllerAction = $observer->getEvent()->getControllerAction();
-        
-        /* @var $request Mage_Core_Controller_Request_Http */
-        $request = $controllerAction->getRequest();
-        /* @var $response Mage_Core_Controller_Response_Http */
-        $response = $controllerAction->getResponse();
-        
+        $response = $controllerAction->getResponse(); /* @var $response Mage_Core_Controller_Response_Http */
         $fullActionName = $controllerAction->getFullActionName();
         
         $lifetime = $helper->isCacheableAction($fullActionName);
         
-        if (!$lifetime) {
-            return $this;
+        if ($lifetime) {
+	        $response->setHeader('X-Magento-Lifetime', $lifetime, true); // Only for debugging and information
+	        $response->setHeader('X-Magento-Action', $fullActionName, true); // Only for debugging and information
+	        $response->setHeader('Cache-Control', 'max-age='. $lifetime, true);
+	        $response->setHeader('aoestatic', 'cache', true);
         }
-        
-        $response->setHeader('X-Magento-Lifetime', $lifetime, true); // Only for debugging and information
-        $response->setHeader('X-Magento-Action', $fullActionName, true); // Only for debugging and information
-        $response->setHeader('Cache-Control', 'max-age='. $lifetime, true);
-        $response->setHeader('aoestatic', 'cache', true);
 		
         return $this;
     }
