54b1b5e8fc3f7cb4b48b60bdfbe6d0c1b11395e5
Fabrizio Branca <mail@fabrizio-branca.de>
2011-09-10 00:23:12 +0200

    Added a placeholder block that acts like a wrapper for the placeholder content

---
+++ code/Block/Placeholder.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Placeholder
+ *
+ * @author Fabrizio Branca
+ * @author https://github.com/maggedotno
+ */
+class Aoe_Static_Block_Placeholder extends Mage_Core_Block_Template
+{
+
+	/**
+	 * @var string default template of this block
+	 */
+	protected $_template = 'aoestatic/placeholder.phtml';
+
+}
