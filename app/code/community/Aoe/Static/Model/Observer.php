331032a4fe4da79c6abd3fc860e486d767dc61fe
thebod <bastian.ike@aoemedia.de>
2013-11-21 12:55:44 +0100

    fixed X

--- app/code/community/Aoe/Static/Model/Observer.php
+++ app/code/community/Aoe/Static/Model/Observer.php
@@ -348,4 +348,16 @@ class Aoe_Static_Model_Observer
         }
         return $this;
     }
+
+    public function controllerActionPredispatchAdminhtmlCacheMassRefresh(Varien_Event_Observer $observer)
+    {
+        /** @var Mage_Core_Controller_Request_Http $request */
+        $request = $observer->getRequest();
+
+        if (strpos($request->getParam('types'), 'aoestatic') !== false) {
+            foreach (Mage::helper('aoestatic')->purgeAll() as $message) {
+                Mage::getSingleton('adminhtml/session')->addNotice($message);
+            }
+        }
+    }
 }
