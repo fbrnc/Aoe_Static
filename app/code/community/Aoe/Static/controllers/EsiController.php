959cc9e8296a03f433c260cdc8f556d0ee51da18
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-08-07 21:36:47 +0000

    Added support for caching (not tested yet)

--- app/code/community/Aoe/Static/controllers/EsiController.php
+++ app/code/community/Aoe/Static/controllers/EsiController.php
@@ -18,6 +18,15 @@ class Aoe_Static_EsiController extends Mage_Core_Controller_Front_Action
         $blockName = $this->getRequest()->getParam('block');
         $block = $this->getLayout()->getBlock($blockName); /* @var $block Mage_Core_Block_Abstract */
         $response = $block ? $block->toHtml() : 'BLOCK NOT FOUND';
+
+        if ($block && $block->getMaxAge()) {
+            /** @var Aoe_Static_Model_Cache_Control $cacheControl */
+            $cacheControl = Mage::getSingleton('aoestatic/cache_control');
+            $cacheControl->addMaxAge($block->getMaxAge());
+            $cacheControl->addTag($block->getBanTags());
+            $cacheControl->applyCacheHeaders();
+        }
+
         $this->getResponse()->setBody($response);
     }
 }
